ab4eb3ad9d93a9c4c8889a7951bdba71d34be7bd
CarlosAsenjo <carlosasenjo.daw@gmail.com>
2020-02-29 15:31:46 +0100

    fin

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-/********* PETICION AJAX ***********/
-
-function request(url) {
-    return new Promise(function(resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.timeout = 2000;
-        xhr.onreadystatechange = function(e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(xhr.response);
-                } else {
-                    reject(xhr.status);
-                }
-            }
-        };
-        xhr.ontimeout = function() {
-            reject('timeout')
-        };
-        xhr.open('get', url, true);
-        xhr.setRequestHeader("x-api-key", "b2a67d10-2fe7-4cf9-a264-d30611e17dbe");
-        xhr.send();
-    });
-}
 
 /********* BOTON MOSTRAR *********/
 document.getElementById("botonShow").addEventListener("click", () => {
